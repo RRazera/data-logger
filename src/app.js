74e684b34fb2b03aad4189354f9717bbb649d09a
Ricardo <razrazera@gmail.com>
2021-03-29 13:55:02 -0300

    first relevant commit

--- src/app.js
+++ src/app.js
@@ -1,37 +1,49 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // define paths for Express config
 const publicDirPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-// Setup handlebars engine and views location
+// setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // setup static dir to server
 app.use(express.static(publicDirPath))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App',
+        title: 'Data Logger',
         name: 'Ricardo Razera'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About me',
+        title: 'About',
         name: 'Ricardo Razera'
     })
 })
 
-app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'clear',
-        location: 'POA'
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Ricardo Razera',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Ricardo Razera',
+        errorMessage: 'Page not found.'
     })
 })
 
