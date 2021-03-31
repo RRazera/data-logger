d56f37bf49e8501e596db3acab02e14caf91d933
Ricardo <razrazera@gmail.com>
2021-03-31 09:43:01 -0300

    setup mongoose and some resource endpoints

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+require('./db/mongoose')
+const User = require('./models/user')
+const Experiment = require('./models/experiment')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,6 +20,50 @@ hbs.registerPartials(partialsPath)
 
 // setup static dir to server
 app.use(express.static(publicDirPath))
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.post('/experiments', (req, res) => {
+    const experiment = new Experiment(req.body)
+
+    experiment.save().then(() => {
+        res.status(201).send(experiment)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
 
 app.get('', (req, res) => {
     res.render('index', {
