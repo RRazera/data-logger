66d739f63080460ff5f74281ebf74e1bb9464884
Ricardo <razrazera@gmail.com>
2021-03-31 12:46:24 -0300

    created folder routers and restructured the app

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 require('./db/mongoose')
-const User = require('./models/user')
-const Experiment = require('./models/experiment')
+const userRouter = require('./routers/user')
+const experimentRouter = require('./routers/experiment')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -23,47 +23,8 @@ app.use(express.static(publicDirPath))
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
-    const user = new User(req.body)
-
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
-        res.send(users)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id
-
-    User.findById(_id).then((user) => {
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.post('/experiments', (req, res) => {
-    const experiment = new Experiment(req.body)
-
-    experiment.save().then(() => {
-        res.status(201).send(experiment)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
+app.use(userRouter)
+app.use(experimentRouter)
 
 app.get('', (req, res) => {
     res.render('index', {
---
+++ src/routers/experiment.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const Experiment = require('../models/experiment')
+const router = new express.Router()
+
+router.post('/experiments', async (req, res) => {
+    const experiment = new Experiment(req.body)
+
+    try {
+        await experiment.save()
+
+        res.status(201).send(experiment)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.get('/experiments', async (req, res) => {
+    try {
+        const experiments = await Experiment.find({})
+
+        res.send(experiments)
+    } catch (e) {
+        res.status(404).send(e)
+    }
+})
+
+router.get('/experiments/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const experiment = await Experiment.findById(_id)
+
+        if (!experiment) {
+            return res.status(404).send()
+        }
+
+        res.send(experiment)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.patch('/experiments/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!'})
+    }
+    try {
+        const experiment = await Experiment.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+
+        if (!experiment) {
+            return res.status(404).send()
+        }
+
+        res.send(experiment)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/experiments/:id', async (req, res) => {
+    try {
+        const experiment = await Experiment.findByIdAndDelete(req.params.id)
+
+        if (!experiment) {
+            return res.status(404).send()
+        }
+
+        res.send(experiment)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+module.exports = router
